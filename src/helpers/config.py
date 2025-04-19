dfb6e94ec9bf4a9d0dae22c7604630e9dba0cdbf
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-04-19 10:15:01 +0400

    Changes to be committed: 	deleted:    rag-app/lib/python3.8/site-packages/pydantic_settings-2.0.3.dist-info/RECORD 	renamed:    rag-app/lib/python3.8/site-packages/pydantic_settings-2.0.3.dist-info/INSTALLER -> rag-app/lib/python3.8/site-packages/pydantic_settings-2.2.1.dist-info/INSTALLER 	renamed:    rag-app/lib/python3.8/site-packages/pydantic_settings-2.0.3.dist-info/METADATA -> rag-app/lib/python3.8/site-packages/pydantic_settings-2.2.1.dist-info/METADATA 	new file:   rag-app/lib/python3.8/site-packages/pydantic_settings-2.2.1.dist-info/RECORD 	renamed:    rag-app/lib/python3.8/site-packages/pydantic_settings-2.0.3.dist-info/REQUESTED -> rag-app/lib/python3.8/site-packages/pydantic_settings-2.2.1.dist-info/REQUESTED 	renamed:    rag-app/lib/python3.8/site-packages/pydantic_settings-2.0.3.dist-info/WHEEL -> rag-app/lib/python3.8/site-packages/pydantic_settings-2.2.1.dist-info/WHEEL 	renamed:    rag-app/lib/python3.8/site-packages/pydantic_settings-2.0.3.dist-info/licenses/LICENSE -> rag-app/lib/python3.8/site-packages/pydantic_settings-2.2.1.dist-info/licenses/LICENSE 	modified:   rag-app/lib/python3.8/site-packages/pydantic_settings/__init__.py 	modified:   rag-app/lib/python3.8/site-packages/pydantic_settings/__pycache__/__init__.cpython-38.pyc 	modified:   rag-app/lib/python3.8/site-packages/pydantic_settings/__pycache__/main.cpython-38.pyc 	modified:   rag-app/lib/python3.8/site-packages/pydantic_settings/__pycache__/sources.cpython-38.pyc 	modified:   rag-app/lib/python3.8/site-packages/pydantic_settings/__pycache__/utils.cpython-38.pyc 	modified:   rag-app/lib/python3.8/site-packages/pydantic_settings/__pycache__/version.cpython-38.pyc 	modified:   rag-app/lib/python3.8/site-packages/pydantic_settings/main.py 	modified:   rag-app/lib/python3.8/site-packages/pydantic_settings/sources.py 	modified:   rag-app/lib/python3.8/site-packages/pydantic_settings/version.py 	modified:   src/.env.example 	modified:   src/helpers/config.py 	modified:   src/main.py 	modified:   src/requirements.txt 	modified:   src/routes/base.py

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,14 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-
     APP_NAME: str
     APP_VERSION: str
     OPEN_API_KEY: str
-
-
-    class confi:
-        env_file = ".env"
+    
+    model_config = SettingsConfigDict(env_file=".env")
 
 def get_settings():
     return Settings()
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter
 import os
 from helpers.config import get_settings
-
+ 
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"],
@@ -13,6 +13,7 @@ async def welcome():
     app_settings = get_settings()
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
+    
 
     return{
     "app_name": app_name,
