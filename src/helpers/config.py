9b07dfd618ca932317e1a994ab8c0981d514f4d1
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-04-19 11:50:32 +0400

    modified:   src/.env.example 	new file:   src/controllers/BaseController.py 	new file:   src/controllers/DataControllers.py 	modified:   src/controllers/__init__.py 	modified:   src/helpers/config.py 	modified:   src/main.py 	modified:   src/routes/data.py

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,6 +4,9 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPEN_API_KEY: str
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE:  int
+
     
     model_config = SettingsConfigDict(env_file=".env")
 
